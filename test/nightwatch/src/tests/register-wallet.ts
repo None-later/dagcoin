0876b14358c7138b6982c183a443d0b6662b92b8
Viljar Võidula <viljar@testreel.com>
2018-07-04 16:05:10 +0300

    Added files to gitignore

--- test/nightwatch/src/tests/register-wallet.ts
+++ test/nightwatch/src/tests/register-wallet.ts
@@ -2,22 +2,21 @@ import {NightWatchClient} from 'nightwatch';
 
 // let recipientAddress: string;
 
-
 export default {
 	'Test mobile': (client: NightWatchClient): void => {
 		// const global: NightWatchClient = client.page.globalPage();
-		const	platform : any = client.options.desiredCapabilities.platformName
+		const platform: any = client.options.desiredCapabilities.platformName;
 		client.useXpath();
 
-		switch(platform){
+		switch (platform) {
 			case 'Android':
 				client.setContext('WEBVIEW_org.dagcoin.client');
-					client.contexts((context)=> {
-					client.setContext(context.value[1])
-					});
-					break;
-			default: console.log('Desktop plaform - no need to change context');	
-
+				client.contexts(context => {
+					client.setContext(context.value[1]);
+				});
+				break;
+			default:
+				console.log('Desktop plaform - no need to change context');
 		}
 		client.waitForElementVisible(
 			'//*[@id="mainSection"]/section/div/div/div[1]/ks-swiper-container/div/div[2]/div[1]/div[1]',
